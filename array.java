bbd41a5e7a95f8978c419ff57d132b30cb7fbbb1
Hrushikesh Bhoir <rushikesh72@gmail.com>
2026-03-08 13:26:07 +0530

    Array Java practice program

---
+++ array.java
@@ -0,0 +1,23 @@
+public class array {
+
+    public static void main(String[] args) {
+        
+        // int nums1[]={5,7,9};
+
+        // nums1[1]=10;  //to change the value 
+        // System.out.println(nums1[1]);
+
+        int nums[]= new int[3];
+
+         nums[0]=5;
+         nums[1]=7;
+         nums[2]=10;
+         
+
+        for(int i=0; i<3; i++)
+        {
+            System.out.println("Value is:"+i);
+        }
+    }
+    
+}
