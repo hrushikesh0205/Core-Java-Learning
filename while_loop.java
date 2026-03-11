032fd019359c9379b6dfce1d138ec57bd41a7411
Hrushikesh Bhoir <rushikeshbhoir72@gmail.com>
2026-03-11 19:02:47 +0530

    while loop example

---
+++ while_loop.java
@@ -0,0 +1,16 @@
+public class while_loop {
+
+    public static void main(String[] args) {
+        
+        int i=1;
+
+        while(i<=4){
+
+            System.out.println("Hello"+i);
+            i++;
+        }
+
+        System.out.println("Done"+i);
+    }
+    
+}
