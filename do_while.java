234032a457acb9135f815b4f5055b1c84dcb0f35
Hrushikesh Bhoir <rushikeshbhoir72@gmail.com>
2026-03-11 19:01:16 +0530

    Do while loop example

---
+++ do_while.java
@@ -0,0 +1,14 @@
+public class do_while {
+
+    public static void main(String[] args) {
+        
+        int i=1;
+        do{
+
+            System.out.println("Hello students!"+i);
+            i++;
+        }
+        while(i<=4);
+    }
+    
+}
