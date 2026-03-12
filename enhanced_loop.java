e1fc939bbc6b4a3f95084c9057ae78aea92cb13f
Hrushikesh Bhoir <rushikeshbhoir72@gmail.com>
2026-03-12 19:03:30 +0530

    Enhanced loop example

---
+++ enhanced_loop.java
@@ -0,0 +1,14 @@
+public class enhanced_loop {
+
+    public static void main(String[] args) {
+        
+    int nums[]={5,10,15,20};
+
+    for(int num: nums)
+    {
+          System.out.println("Array is:"+num);
+           
+    }
+    
+}
+}
