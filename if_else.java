a9cb3fad2231ace353ba7a2c4c6df33136d36478
Hrushikesh Bhoir <rushikeshbhoir72@gmail.com>
2026-03-13 06:50:26 +0530

    if else statement example

---
+++ if_else.java
@@ -0,0 +1,16 @@
+public class if_else {
+
+    public static void main(String[] args) {
+        
+        int age=20;
+        if(age>18)
+        {
+            System.out.println("You are eligible for vote!");
+        }
+        else
+        {
+            System.out.println("Yoou are not eligible for vote!");
+        }
+    }
+    
+}
