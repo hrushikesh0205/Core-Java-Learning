ec3876a665fbcc6d9906f096d795ea07d5a5433d
Hrushikesh Bhoir <rushikeshbhoir72@gmail.com>
2026-03-13 06:52:37 +0530

    switch case  example

---
+++ switch_case.java
@@ -0,0 +1,39 @@
+public class switch_case {
+    
+    public static void main(String[] args) {
+        
+        int num=5;
+
+        switch (num) {
+            case 1:
+                System.out.println("MONDAY");
+                break;
+        
+            case 2:
+                System.out.println("TUESDAY");
+                break;
+
+            case 3:
+                System.out.println("WEDNESDAY");
+                break;
+
+            case 4:
+                System.out.println("THURSDAY");
+                break;
+
+            case 5:
+                System.out.println("FRIDAY");
+                break;
+
+            case 6:
+                System.out.println("SATURDAY");
+                break;
+      
+            case 7:
+                System.out.println("SUNDAY");
+            default:
+                System.out.println("Invalid day!!");
+                break;
+        }
+    }
+}
